600bd6ee0e71a6fbc02201bc6f6d5e6e0e980878
Nicolas <nicolas.lacaze@gmail.com>
2019-02-21 23:23:57 +0100

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import countries from './data/countries.json';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CountryDetail from './components/CountryDetail';
 
 class App extends Component {
+  state = {
+    countries,
+  }
+
+  listAllCountries = () => {
+    const { countries } = this.state;
+    return countries.map((country, index) => {
+      return (
+        <Link to={`/${country.cca3}`}>
+          <li key={index} className="list-group-item list-group-item-action">
+            <span>{country.flag}</span>
+            {country.name.common}
+          </li>
+        </Link>
+      );
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="navbar navbar-light bg-light">
+          <h1 className="navbar-brand">
+            Wiki countries
+          </h1>
+        </nav>
+        
+        <div className="container">
+          <Router>
+            <div className="row">
+              <div className="col-5 sidebar">
+                <ul className="list-group scroll">
+                  {this.listAllCountries()}
+                </ul>
+              </div>
+              <div className="col-7 main">
+                <Route exact path="/:id" component={CountryDetail}/>    
+              </div>
+            </div>
+          </Router>
+        </div>
       </div>
     );
   }
