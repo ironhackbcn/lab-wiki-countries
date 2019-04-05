a378a0720e2348311a128c577758eab90bd70fd1
Oriol Carbo <oriol.carbo@gmail.com>
2019-04-05 20:18:02 +0200

    done

---
+++ src/pages/ContryDetail.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import countries from '../data/countries.json';
+
+
+// Shows each country detail
+class Country extends Component {
+
+  render() {
+    const countryID = this.props.match.params;
+    const [countryMatch] = countries.filter((country) => {
+      return countryID.cca3 === country.cca3;
+    })
+    
+    return (
+      <div className="col-">
+        <div className="list-group">
+          <h2>{countryMatch.name.common}</h2>   
+          <hr/>  
+          <h4>Capital: {countryMatch.capital}</h4>
+          <hr/>
+          <h4>Area: {countryMatch.area} km<sup>2</sup></h4>   
+          <ul>
+            {countryMatch.borders.map((border, index) => {
+              const [borderCountry] = countries.filter(country => {
+                return country.cca3 === border;
+              })
+              return <li key={index}>
+              <Link to={`/country/${border}`}>
+                {borderCountry.name.common}
+              </Link></li>
+            })}
+          </ul> 
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Country;
