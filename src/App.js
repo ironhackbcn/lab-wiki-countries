418082f787c92275c695350d63bb6be39d9678ee
johan Bautista <jsbparra@gmail.com>
2019-10-09 21:27:50 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country';
+import CountryDetail from './components/CountryDetail';
+// import Nav from './components/nav';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="row">
+          <Route path="/" component={Country}  className="contain1"/>
+          <Route path="/country/:cca3" component={CountryDetail}  className="contain2"/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Country.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import countries from '../data/countries'
+
+
+class Country extends Component {
+  state={
+    countries: countries
+  }
+  render() {
+    const { countries } = this.state;
+    return (
+      <div className="col-5">
+        <ul className="list-group">
+        {countries.map((country, index) => {
+          return(<Link className="list-group-item list-group-item-action"
+          key={index} to={`/country/${country.cca3}`}>{country.flag}{country.name.common}</Link>)
+        })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Country;
