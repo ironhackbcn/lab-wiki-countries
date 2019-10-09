d120b1f4466f7831ed8c7e76e170c6917b798f5c
johan Bautista <jsbparra@gmail.com>
2019-10-09 21:29:07 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import logo from './logo.svg';
 import './App.css';
 import Country from './components/Country';
 import CountryDetail from './components/CountryDetail';
-// import Nav from './components/nav';
 
 class App extends Component {
   render() {
