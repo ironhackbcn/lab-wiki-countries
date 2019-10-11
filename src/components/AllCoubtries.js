f61ad22fbcaa46caf1909bfc65f704a5d3bc2c49
Mario Espada <mario.espada77@gmail.com>
2019-10-11 02:10:25 +0200

    ADD list all countries

---
+++ src/components/AllCoubtries.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import countries from "../data/countries.json";
+
+class AllCoubtries extends Component {
+    render() {
+        return (
+            <div>
+                {
+                    countries.map((country, index) =>{
+                        return(
+                            <Link
+                            key={`country-${index}`}
+                            to={country.cca3}
+                            >
+                                <div className="flag-name">
+                                <p>{country.flag}</p>
+                                <p>{country.name.common}</p>
+                                </div>
+                            </Link>
+  
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default AllCoubtries;
