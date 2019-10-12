c38815acf7129a5af789dbb2fd7553d3eec0ad81
Olga Zinoveva <ozinka81@gmail.com>
2019-10-12 22:15:07 +0200

    intital commit

---
+++ src/components/CountryDetail.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ContryDetail extends Component {
+  render() {
+    return <div className="App">hello Olga</div>;
+  }
+}
+
+export default ContryDetail;
