a61cc228cce520b6b9e0a19a72f5bcaadde2fb83
Olga Zinoveva <ozinka81@gmail.com>
2019-10-13 22:04:09 +0200

    done80%

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
 import countries from "./data/countries.json";
-import ContryDetail from "./components/CountryDetail";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import CountryDetail from "./components/CountryDetail";
+import ListContries from "./components/ListContries";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
 
 class App extends Component {
   render() {
-    return <div className="App">hello Olga</div>;
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-primary mb-3">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              WikiCountries
+            </a>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={ListContries} />
+          <Route path="/:id" component={CountryDetail} />
+        </Switch>
+      </div>
+    );
   }
 }
-
 export default App;
--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,9 +1,62 @@
 import React, { Component } from "react";
+import countries from "../data/countries.json";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
 
-class ContryDetail extends Component {
-  render() {
-    return <div className="App">hello Olga</div>;
-  }
-}
+const CountryDetail = props => {
+  const findedCountry = countryID => {
+    const theCountry = oneCountry => {
+      return oneCountry.cca3 === countryID;
+    };
+    return countries.find(theCountry);
+  };
 
-export default ContryDetail;
+  const { params } = props.match;
+  const foundCountry = findedCountry(params.id);
+
+  return (
+    <div className="col-7">
+      <h1>{foundCountry.name.common}</h1>
+      <table className="table">
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td>Capital</td>
+            <td>{foundCountry.capital}</td>
+          </tr>
+          <tr>
+            <td>Area</td>
+            <td>
+              {foundCountry.area} km
+              <sup>2</sup>
+            </td>
+          </tr>
+          <tr>
+            <td>Borders</td>
+            <td>
+              <ul>
+                {foundCountry.borders.map((cca3, index) => {
+                  const country = findedCountry(cca3).name.common;
+                  return (
+                    <li key={`${cca3}-${index}`}>
+                      <NavLink to={`/${cca3}`} activeStyle={{ color: "green" }}>
+                        {country}
+                      </NavLink>
+                    </li>
+                  );
+                })}
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CountryDetail;
